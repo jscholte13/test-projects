83465dbb278c629cd730d7a78007a1e2831067aa
jscholte <jscholte@spotx.tv>
2018-09-17 16:28:10 +0200

    Initial Commit

---
+++ Johnno first app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Johnno first app
+//
+//  Created by johnno  scholte on 17/09/2018.
+//  Copyright © 2018 Js_design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
